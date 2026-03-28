5feec8bbf9e5a34430b04de864f9bf0c8a226f10
George Fayek <georgefayekwilliam@gmail.com>
2026-03-28 14:05:18 +0200

    Q01

--- G-NET-12-Adv01/Program.cs
+++ G-NET-12-Adv01/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace G_NET_12_Adv01
+﻿using System.Timers;
+
+namespace G_NET_12_Adv01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+            //Q1: What is a generic class? Why use generics?
+
+            //Generics allow you to define type - safe classes, interfaces, methods, and delegates without committing to a specific data type until the code is used.
+
+            //Before generics, we had two bad choices: duplicate code for each type, or use object and lose type safety + performance.
+
+            //Benefits of Generics
+            //Benefit Description
+            //Type Safety Compile-time type checking
+            //Performance No boxing / unboxing for value types
+            //Code Reuse  One implementation for all types
+            //IntelliSense Better IDE support and discovery
+
+
+
+            #endregion
         }
     }
 }
